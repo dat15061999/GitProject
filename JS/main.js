a04d54bb2c86c8815d9a3fe36165b4aeb5e6d851
dat15061999 <datphamvan49@gmail.com>
2023-05-09 20:35:36 +0700

    add js

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,13 @@
-var x = [1,3,5];
-var y = x.length;
-console.log(y);
+function showmesage(){
+    alert('Xin chao');
+}
+function showmesage1(){
+    document.write('Xin chao');    
+}
+
+function showmesage2(){
+    document.getElementById('demo').innerHTML='Xin cam on';
+}
+function showmesage3(){
+    console.log('xin cam on');
+}
