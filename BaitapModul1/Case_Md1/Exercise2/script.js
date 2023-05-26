ede03c44436a7d715b19329e84a06d3d885071dc
dat15061999 <datphamvan49@gmail.com>
2023-05-26 22:21:19 +0700

    add function

--- BaitapModul1/Case_Md1/Exercise2/script.js
+++ BaitapModul1/Case_Md1/Exercise2/script.js
@@ -6,3 +6,9 @@ const adressctn = document.querySelector('#closeAdress');
 adressctn.addEventListener('click', function () {
     document.querySelector('.adress-form').style.display = 'none';
 })
+
+//1. Tạo đối tượng User có 2 thuộc tính là username và password
+//2. Tạo const accountDemo type = User
+//3. Validate username từ 5 đến 30 ký tự
+//4, Mk từ 6-18 ký tự
+//5. Check nếu login request đúng thì đẩy qua trang home
